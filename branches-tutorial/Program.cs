083192c9c0bc3461c632d0d5cbd5156cc9492921
alex george <alexandreantoinegeorge@gmail.com>
2019-03-06 20:16:10 -0500

    cours boucles et conditions

--- branches-tutorial/Program.cs
+++ branches-tutorial/Program.cs
@@ -4,12 +4,58 @@ namespace BranchesAndLoops
 {
     class Program
     {
-        static void Main(string[] args)
+        static void ExploreIf()
         {
             int a = 5;
             int b = 3;
+            int c = 1;
             if (a + b > 10)
-                Console.WriteLine("The answer is greater than 10.");
+            {
+                Console.WriteLine("The answer is greater than 10");
+            }
+            else
+            {
+                Console.WriteLine("The answer is not greater than 10");
+            }
+
+            if ((a + b + c > 10) && (a > b))
+            {
+                Console.WriteLine("The answer is greater than 10");
+                Console.WriteLine("And the first number is greater than the second");
+            }
+            else
+            {
+                Console.WriteLine("The answer is not greater than 10");
+                Console.WriteLine("Or the first number is not greater than the second");
+            }
+
+            if ((a + b + c > 10) || (a > b))
+            {
+                Console.WriteLine("The answer is greater than 10");
+                Console.WriteLine("Or the first number is greater than the second");
+            }
+            else
+            {
+                Console.WriteLine("The answer is not greater than 10");
+                Console.WriteLine("And the first number is not greater than the second");
+            }            
+        }
+
+        static void IntergerThreeDivisible()
+        {
+            int sum = 0;
+            for(int number = 0; number <= 20; number++)
+            {
+                if(number % 3 == 0)
+                {
+                    sum += number;
+                } 
+            }
+            Console.WriteLine(sum);
+        }
+        static void Main(string[] args)
+        {
+            IntergerThreeDivisible();
         }
     }
 }
