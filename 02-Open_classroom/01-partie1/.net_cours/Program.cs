2d212a3d0ba991096d23234dc1635c57238d7a2d
AlexandreGeorge <alexandreantoinegeorge@gmail.com>
2019-03-09 10:38:20 -0500

    tp bon weekend

---
+++ 02-Open_classroom/01-partie1/.net_cours/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _net_cours
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 02-Open_classroom/01-partie1/tpbonwk/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace tpbonwk
+{
+    class Program
+    {
+        static void message()
+        {
+            string userName = System.Environment.UserName;
+            DateTime timeNow = DateTime.Now;
+            if (timeNow.DayOfWeek.ToString() == "Saturday" || timeNow.DayOfWeek.ToString() == "Sunday")
+            {
+                Console.WriteLine($"Bon weekend {userName}");
+            }
+            else 
+            {   
+                if (timeNow.Hour > 9 && timeNow.Hour < 18)
+                {
+                    Console.WriteLine($"Bonjour {userName}");
+                }
+                else
+                {
+                     Console.WriteLine($"Bonsoir {userName}");
+                }            
+            }
+        }
+        static void Main(string[] args)
+        {
+            message();
+        }
+    }
+}
